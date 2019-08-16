b72d31f1d7efc8e913dc33a58d6c28111bdffb10
Jonathan Dickert <jon.p.dickert@gmail.com>
2019-08-16 11:50:09 -0400

    auto update

--- c++/tools/console.hpp
+++ c++/tools/console.hpp
@@ -77,58 +77,61 @@ class Console {
 
     Console() = default;
 
-    const char* setOpt(ForgroundColor c) {
+    constexpr inline const char* setOpt(ForgroundColor c) {
       switch(c) {
-	case ForgroundColor::Reset:   return CODE_FORGROUND_RESET;
-	case ForgroundColor::Black:   return CODE_FORGROUND_BLACK;
-	case ForgroundColor::Red:     return CODE_FORGROUND_RED;
-	case ForgroundColor::Green:   return CODE_FORGROUND_GREEN;
-	case ForgroundColor::Yellow:  return CODE_FORGROUND_YELLOW;
-	case ForgroundColor::Blue:    return CODE_FORGROUND_BLUE;
-	case ForgroundColor::Magenta: return CODE_FORGROUND_MAGENTA;
-	case ForgroundColor::Cyan:    return CODE_FORGROUND_CYAN;
-	case ForgroundColor::White:   return CODE_FORGROUND_WHITE;
+	case ForgroundColor::Reset:	return CODE_FORGROUND_RESET;
+	case ForgroundColor::Black:	return CODE_FORGROUND_BLACK;
+	case ForgroundColor::Red:	return CODE_FORGROUND_RED;
+	case ForgroundColor::Green:	return CODE_FORGROUND_GREEN;
+	case ForgroundColor::Yellow:	return CODE_FORGROUND_YELLOW;
+	case ForgroundColor::Blue:	return CODE_FORGROUND_BLUE;
+	case ForgroundColor::Magenta:	return CODE_FORGROUND_MAGENTA;
+	case ForgroundColor::Cyan:	return CODE_FORGROUND_CYAN;
+	case ForgroundColor::White:	return CODE_FORGROUND_WHITE;
+	default:			return "";
       }
     }
 
-    const char* setOpt(BackgroundColor c) {
+    constexpr inline const char* setOpt(BackgroundColor c) {
       switch(c) {
-	case BackgroundColor::Reset: return CODE_BACKGROUND_RESET;
-	case BackgroundColor::Black: return CODE_BACKGROUND_BLACK;
-	case BackgroundColor::Red: return CODE_BACKGROUND_RED;
-	case BackgroundColor::Green: return CODE_BACKGROUND_GREEN;
-	case BackgroundColor::Yellow: return CODE_BACKGROUND_YELLOW;
-	case BackgroundColor::Blue: return CODE_BACKGROUND_BLUE;
-	case BackgroundColor::Magenta: return CODE_BACKGROUND_MAGENTA;
-	case BackgroundColor::Cyan: return CODE_BACKGROUND_CYAN;
-	case BackgroundColor::White: return CODE_BACKGROUND_WHITE;
+	case BackgroundColor::Reset: 	return CODE_BACKGROUND_RESET;
+	case BackgroundColor::Black: 	return CODE_BACKGROUND_BLACK;
+	case BackgroundColor::Red:	return CODE_BACKGROUND_RED;
+	case BackgroundColor::Green: 	return CODE_BACKGROUND_GREEN;
+	case BackgroundColor::Yellow:	return CODE_BACKGROUND_YELLOW;
+	case BackgroundColor::Blue:	return CODE_BACKGROUND_BLUE;
+	case BackgroundColor::Magenta:	return CODE_BACKGROUND_MAGENTA;
+	case BackgroundColor::Cyan:	return CODE_BACKGROUND_CYAN;
+	case BackgroundColor::White:	return CODE_BACKGROUND_WHITE;
+	default:			return "";
       }
     }
 
-    const char* setOpt(FontStyle fs) {
+    constexpr inline const char* setOpt(FontStyle fs) {
       switch(fs) {
-	case FontStyle::Bold: return CODE_BOLD;
-	case FontStyle::Faint: return CODE_FAINT;
-	case FontStyle::Italic: return CODE_ITALIC;
-	case FontStyle::Underline: return CODE_UNDERLINE;
-	case FontStyle::UnderlineOff: return CODE_UNDERLINE_OFF;
-	case FontStyle::CrossedOut: return CODE_CROSSEDOUT;
-	case FontStyle::CrossedOutOff: return CODE_CROSSEDOUT_OFF;
+	case FontStyle::Bold:		return CODE_BOLD;
+	case FontStyle::Faint:		return CODE_FAINT;
+	case FontStyle::Italic:		return CODE_ITALIC;
+	case FontStyle::Underline:	return CODE_UNDERLINE;
+	case FontStyle::UnderlineOff:	return CODE_UNDERLINE_OFF;
+	case FontStyle::CrossedOut:	return CODE_CROSSEDOUT;
+	case FontStyle::CrossedOutOff:	return CODE_CROSSEDOUT_OFF;
+	default:			return "";
       }
     }
 
-    const char* setOpt(Attrs a) {
+    constexpr inline const char* setOpt(Attrs a) {
       switch(a) {
-	case Attrs::FullReset: return CODE_FULLRESET;
-	case Attrs::SlowBlink: return CODE_SLOWBLINK;
-	case Attrs::BlinkOff: return CODE_BLINK_OFF;
+	case Attrs::FullReset:		return CODE_FULLRESET;
+	case Attrs::SlowBlink:		return CODE_SLOWBLINK;
+	case Attrs::BlinkOff:		return CODE_BLINK_OFF;
+	default:			return "";
       }
     }
 
     template<typename... Args>
     void write(Args&&... args) {
-      ((std::cout << std::forward<Args>(args)), ...);
-      std::cout << "\x1b[m";
+      ((std::cout << std::forward<Args>(args)), ...) << setOpt(Attrs::FullReset);
     }
 };
 
