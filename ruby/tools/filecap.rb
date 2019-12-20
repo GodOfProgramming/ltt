728b7eccd026e2d3d04843eca0af31b01ceb1382
godofprogramming <jon.p.dickert@gmail.com>
2019-12-20 04:09:48 -0500

    auto update -> added ruby tools

---
+++ ruby/tools/filecap.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+require 'rails'
+
+cur_dir = Dir.pwd
+
+ARGV.each do |arg|
+    orig = "#{cur_dir}/#{arg}"
+    new = "#{cur_dir}/#{File.basename(arg, File.extname(arg)).camelize}#{File.extname(arg)}"
+    puts "renaming #{orig} -> #{new}"
+    File.rename(orig, new)
+end
+
