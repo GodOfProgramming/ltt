2d02664a940d2e841eb1b662087d7a41ce34b5bf
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-02-09 15:22:32 -0500

    quick commit

--- c++/lib/utility/include/strings.hpp
+++ c++/lib/utility/include/strings.hpp
@@ -3,12 +3,22 @@
 
 namespace utility
 {
-  inline std::vector<std::string> StringSplit(const std::string str, const std::string regex)
+
+  inline std::vector<std::string> StringSplit(const std::string str, const std::regex delim);
+
+  inline std::vector<std::string> StringSplit(const std::string str, const std::string regex);
+
+  inline std::vector<std::string> StringSplit(const std::string str, const std::regex delim)
   {
-    std::regex delim(regex);
     std::sregex_token_iterator start(str.begin(), str.end(), delim, -1);
     std::sregex_token_iterator end;
     return std::move(std::vector<std::string>(start, end));
+  }
+
+  inline std::vector<std::string> StringSplit(const std::string str, const std::string regex)
+  {
+    std::regex delim(regex);
+    return StringSplit(str, delim);
   }
 }  // namespace utility
 
--- c++/lib/utility/spec/strings.spec.cpp
+++ c++/lib/utility/spec/strings.spec.cpp
@@ -19,5 +19,25 @@ Eval(Strings)
         Expect(splits.size()).toEqual(3u);
       });
     });
+
+    Context("overload works the same way", [] {
+      Context("one token", [] {
+        It("returns size of 1", [] {
+          std::string str = "somelongstring";
+          std::regex regex(R"(\s+)");
+          auto splits = utility::StringSplit(str, regex);
+          Expect(splits.size()).toEqual(1u);
+        });
+      });
+
+      Context("more than one token", [] {
+        It("returns the expected size", [] {
+          std::string str = "three small\twords";
+          std::regex regex(R"(\s+)");
+          auto splits = utility::StringSplit(str, regex);
+          Expect(splits.size()).toEqual(3u);
+        });
+      });
+    });
   });
 }
