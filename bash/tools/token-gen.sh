0fa737369b0d45d2a42047735187bf39ae89f41a
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-02-18 09:29:10 -0500

    quick commit

--- bash/tools/token-gen.sh
+++ bash/tools/token-gen.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-dist/tokens -token=new -expires=20s 		\
-	-privatekey="$RELAY_PRIVATE_KEY" 			\
-	-clientaddr="$NN_CLIENT_ADDR" 				\
-	-clientpublickey="$RELAY_PUBLIC_KEY" 	\
-	-relayaddr="$RELAY_ADDRESS" 					\
-	-relaypublickey="$RELAY_PUBLIC_KEY" 	\
-	-serveraddr="$NN_SERVER_ADDR" 				\
-	-serverpublickey="$RELAY_PUBLIC_KEY" > "$NN_FAKE_PACKET_BIN"
+if [[ "$#" -eq "1" ]]; then
+	echo "Generating full token"
+	dist/tokens -token=new -expires=20s 		\
+		-privatekey="$RELAY_PRIVATE_KEY" 			\
+		-clientaddr="$NN_CLIENT_ADDR" 				\
+		-clientpublickey="$RELAY_PUBLIC_KEY" 	\
+		-relayaddr="$RELAY_ADDRESS" 					\
+		-relaypublickey="$RELAY_PUBLIC_KEY" 	\
+		-serveraddr="$NN_SERVER_ADDR" 				\
+		-serverpublickey="$RELAY_PUBLIC_KEY" > "$NN_FAKE_PACKET_BIN"
+else
+	echo "Generating token without client"
+	dist/tokens -token=new -expires=20s 		\
+		-privatekey="$RELAY_PRIVATE_KEY" 			\
+		-relayaddr="$RELAY_ADDRESS" 					\
+		-relaypublickey="$RELAY_PUBLIC_KEY" 	\
+		-serveraddr="$NN_SERVER_ADDR" 				\
+		-serverpublickey="$RELAY_PUBLIC_KEY" > "$NN_FAKE_PACKET_BIN"
+fi
 
