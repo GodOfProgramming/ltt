8179151a8b48fdf77a38e1db2767645e6fa1f6c3
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-03-03 16:15:15 -0500

    quick commit

--- c++/tools/udp-flood/include/defines.h
+++ c++/tools/udp-flood/include/defines.h
@@ -1,31 +1,31 @@
 #define ERRORF(x...) fprintf(stderr, x)
 
-#define FATAL(x...)                                                            \
-	do {                                                                   \
-		ERRORF("[-] PROGRAM ABORT : " x);                              \
-		ERRORF("\n\tLocation : %s(), %s:%u\n\n", __FUNCTION__,         \
-		       __FILE__, __LINE__);                                    \
-		exit(EXIT_FAILURE);                                            \
-	} while (0)
+#define FATAL(x...)                                                             \
+  do {                                                                          \
+    ERRORF("[-] PROGRAM ABORT : " x);                                           \
+    ERRORF("\n\tLocation : %s(), %s:%u\n\n", __FUNCTION__, __FILE__, __LINE__); \
+    exit(EXIT_FAILURE);                                                         \
+  } while (0)
 
-#define PFATAL(x...)                                                           \
-	do {                                                                   \
-		ERRORF("[-] SYSTEM ERROR : " x);                               \
-		ERRORF("\n\tLocation : %s(), %s:%u\n", __FUNCTION__, __FILE__, \
-		       __LINE__);                                              \
-		perror("      OS message ");                                   \
-		ERRORF("\n");                                                  \
-		exit(EXIT_FAILURE);                                            \
-	} while (0)
+#define PFATAL(x...)                                                          \
+  do {                                                                        \
+    ERRORF("[-] SYSTEM ERROR : " x);                                          \
+    ERRORF("\n\tLocation : %s(), %s:%u\n", __FUNCTION__, __FILE__, __LINE__); \
+    perror("      OS message ");                                              \
+    ERRORF("\n");                                                             \
+    exit(EXIT_FAILURE);                                                       \
+  } while (0)
 
 #define TIMESPEC_NSEC(ts) ((ts)->tv_sec * 1000000000ULL + (ts)->tv_nsec)
-#define TIMEVAL_NSEC(ts)                                                       \
-	((ts)->tv_sec * 1000000000ULL + (ts)->tv_usec * 1000ULL)
-#define NSEC_TIMESPEC(ns)                                                      \
-	(struct timespec) { (ns) / 1000000000ULL, (ns) % 1000000000ULL }
-#define NSEC_TIMEVAL(ns)                                                       \
-	(struct timeval)                                                       \
-	{                                                                      \
-		(ns) / 1000000000ULL, ((ns) % 1000000000ULL) / 1000ULL         \
-	}
+#define TIMEVAL_NSEC(ts) ((ts)->tv_sec * 1000000000ULL + (ts)->tv_usec * 1000ULL)
+#define NSEC_TIMESPEC(ns)                      \
+  (struct timespec)                            \
+  {                                            \
+    (ns) / 1000000000ULL, (ns) % 1000000000ULL \
+  }
+#define NSEC_TIMEVAL(ns)                                   \
+  (struct timeval)                                         \
+  {                                                        \
+    (ns) / 1000000000ULL, ((ns) % 1000000000ULL) / 1000ULL \
+  }
 #define MSEC_NSEC(ms) ((ms)*1000000ULL)
--- c++/tools/udp-flood/src/main.cxx
+++ c++/tools/udp-flood/src/main.cxx
@@ -1,18 +1,185 @@
 #include "common.h"
 #include "defines.h"
 
-struct NetAddr
+namespace net
 {
-  int ipver;
-  sockaddr_in sin4;
-  sockaddr_in6 sin6;
-  sockaddr* addr;
-  int addr_len;
-};
+  struct Addr
+  {
+    int ipver;
+    sockaddr_in sin4;
+    sockaddr_in6 sin6;
+    sockaddr* addr;
+    int addr_len;
+  };
+
+  void set_buffer_size(int cd, int max, int send)
+  {
+    int i, flag;
+
+    if (send) {
+      flag = SO_SNDBUF;
+    } else {
+      flag = SO_RCVBUF;
+    }
+
+    for (i = 0; i < 10; i++) {
+      int bef;
+      socklen_t size = sizeof(bef);
+      if (getsockopt(cd, SOL_SOCKET, flag, &bef, &size) < 0) {
+        PFATAL("getsockopt(SOL_SOCKET)");
+        break;
+      }
+      if (bef >= max) {
+        break;
+      }
+
+      size = bef * 2;
+      if (setsockopt(cd, SOL_SOCKET, flag, &size, sizeof(size)) < 0) {
+        // don't log error, just break
+        break;
+      }
+    }
+  }
+
+  void get_host_by_name(Addr& shost, const char* host, int port)
+  {
+    memset(&shost, 0, sizeof(shost));
+
+    in_addr in_addr;
+    in6_addr in6_addr;
+
+    if (inet_pton(AF_INET, host, &in_addr) == 1) {
+      shost.ipver = 4;
+      shost.addr = (sockaddr*)&shost.sin4;
+      shost.addr_len = sizeof(shost.sin4);
+      shost.sin4.sin_family = AF_INET;
+      shost.sin4.sin_port = htons(port);
+      shost.sin4.sin_addr = in_addr;
+    } else if (inet_pton(AF_INET6, host, &in6_addr) == 1) {
+      shost.ipver = 6;
+      shost.addr = (sockaddr*)&shost.sin6;
+      shost.addr_len = sizeof(shost.sin4);
+      shost.sin6.sin6_family = AF_INET6;
+      shost.sin6.sin6_port = htons(port);
+      shost.sin6.sin6_addr = in6_addr;
+    } else {
+      FATAL("gethostbyname, bad addr: '%s'", host);
+      return;
+    }
+  }
+
+  void parse_addr(Addr& netaddr, const char* addr)
+  {
+    char* colon = const_cast<char*>(strrchr(addr, ':'));
+    if (colon == NULL) {
+      FATAL("You forgot to specify port");
+    }
+
+    int port = atoi(colon + 1);
+    if (port < 0 || port > 65535) {
+      FATAL("Invalid port number %d", port);
+    }
+
+    char host[255];
+    int addr_len = colon - addr > 254 ? 254 : colon - addr;
+
+    strncpy(host, addr, addr_len);
+
+    host[addr_len] = '\0';
+    get_host_by_name(netaddr, host, port);
+  }
+
+  const char* addr_to_str(Addr& addr)
+  {
+    char dst[INET6_ADDRSTRLEN + 1];
+    int port = 0;
+
+    switch (addr.ipver) {
+      case 4: {
+        inet_ntop(AF_INET, &addr.sin4.sin_addr, dst, INET6_ADDRSTRLEN);
+        port = ntohs(addr.sin4.sin_port);
+      } break;
+      case 16: {
+        inet_ntop(AF_INET6, &addr.sin6.sin6_addr, dst, INET6_ADDRSTRLEN);
+        port = ntohs(addr.sin6.sin6_port);
+      } break;
+      default:
+        dst[0] = '?';
+        dst[1] = 0x00;
+    }
+
+    static char buf[255];
+    snprintf(buf, sizeof(buf), "%s:%i", dst, port);
+    return buf;
+  }
+
+  int bind_udp(Addr& shost, int reuseport)
+  {
+    int sd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sd < 0) {
+      PFATAL("socket()");
+    }
+
+    int one = 1;
+    int r = setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char*)&one, sizeof(one));
+    if (r < 0) {
+      PFATAL("setsockopt(SO_REUSEADDR)");
+    }
+
+    if (reuseport) {
+      one = 1;
+      r = setsockopt(sd, SOL_SOCKET, SO_REUSEPORT, (char*)&one, sizeof(one));
+      if (r < 0) {
+        PFATAL("setsockopt(SO_REUSEPORT)");
+      }
+    }
+
+    if (bind(sd, shost.addr, shost.addr_len) < 0) {
+      PFATAL("bind()");
+    }
+
+    return sd;
+  }
+
+  int connect_udp(Addr& shost, int src_port)
+  {
+    int sd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sd < 0) {
+      PFATAL("socket()");
+    }
+
+    int one = 1;
+    int r = setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char*)&one, sizeof(one));
+    if (r < 0) {
+      PFATAL("setsockopt(SO_REUSEADDR)");
+    }
+
+    if (src_port > 1 && src_port < 65536) {
+      Addr src;
+      memset(&src, 0, sizeof(Addr));
+      char buf[32];
+      snprintf(buf, sizeof(buf), "0.0.0.0:%d", src_port);
+      parse_addr(src, buf);
+      if (bind(sd, src.addr, src.addr_len) < 0) {
+        PFATAL("bind()");
+      }
+    }
+
+    if (-1 == connect(sd, shost.addr, shost.addr_len)) {
+      /* is non-blocking, so we don't get error at that point yet */
+      if (EINPROGRESS != errno) {
+        PFATAL("connect()");
+        return -1;
+      }
+    }
+
+    return sd;
+  }
+}  // namespace net
 
 struct State
 {
-  NetAddr* target_addr;
+  net::Addr* target_addr;
   int packets_in_buf;
   const char* payload;
   int payload_sz;
@@ -25,145 +192,6 @@ struct Thread
   void (*callback)(void* userdata);
   void* userdata;
 };
-
-const char* str_quote(const char* s)
-{
-  static char buf[1024];
-  int r = snprintf(buf, sizeof(buf), "\"%.*s\"", (int)sizeof(buf) - 4, s);
-  if (r >= (int)sizeof(buf)) {
-    buf[sizeof(buf) - 1] = 0;
-  }
-  return buf;
-}
-
-void net_set_buffer_size(int cd, int max, int send)
-{
-  int i, flag;
-
-  if (send) {
-    flag = SO_SNDBUF;
-  } else {
-    flag = SO_RCVBUF;
-  }
-
-  for (i = 0; i < 10; i++) {
-    int bef;
-    socklen_t size = sizeof(bef);
-    if (getsockopt(cd, SOL_SOCKET, flag, &bef, &size) < 0) {
-      PFATAL("getsockopt(SOL_SOCKET)");
-      break;
-    }
-    if (bef >= max) {
-      break;
-    }
-
-    size = bef * 2;
-    if (setsockopt(cd, SOL_SOCKET, flag, &size, sizeof(size)) < 0) {
-      // don't log error, just break
-      break;
-    }
-  }
-}
-
-void net_gethostbyname(NetAddr* shost, const char* host, int port)
-{
-  memset(shost, 0, sizeof(NetAddr));
-
-  in_addr in_addr;
-  in6_addr in6_addr;
-
-  if (inet_pton(AF_INET, host, &in_addr) == 1) {
-    shost->ipver = 4;
-    shost->addr = (sockaddr*)&shost->sin4;
-    shost->addr_len = sizeof(shost->sin4);
-    shost->sin4.sin_family = AF_INET;
-    shost->sin4.sin_port = htons(port);
-    shost->sin4.sin_addr = in_addr;
-  } else if (inet_pton(AF_INET6, host, &in6_addr) == 1) {
-    shost->ipver = 6;
-    shost->addr = (sockaddr*)&shost->sin6;
-    shost->addr_len = sizeof(shost->sin4);
-    shost->sin6.sin6_family = AF_INET6;
-    shost->sin6.sin6_port = htons(port);
-    shost->sin6.sin6_addr = in6_addr;
-  } else {
-    FATAL("inet_pton(%s)", str_quote(host));
-    return;
-  }
-}
-
-void parse_addr(NetAddr* netaddr, const char* addr)
-{
-  char* colon = const_cast<char*>(strrchr(addr, ':'));
-  if (colon == NULL) {
-    FATAL("You forgot to specify port");
-  }
-
-  int port = atoi(colon + 1);
-  if (port < 0 || port > 65535) {
-    FATAL("Invalid port number %d", port);
-  }
-
-  char host[255];
-  int addr_len = colon - addr > 254 ? 254 : colon - addr;
-
-  strncpy(host, addr, addr_len);
-
-  host[addr_len] = '\0';
-  net_gethostbyname(netaddr, host, port);
-}
-
-const char* addr_to_str(NetAddr* addr)
-{
-  char dst[INET6_ADDRSTRLEN + 1];
-  int port = 0;
-
-  switch (addr->ipver) {
-    case 4: {
-      inet_ntop(AF_INET, &addr->sin4.sin_addr, dst, INET6_ADDRSTRLEN);
-      port = ntohs(addr->sin4.sin_port);
-    } break;
-    case 16: {
-      inet_ntop(AF_INET6, &addr->sin6.sin6_addr, dst, INET6_ADDRSTRLEN);
-      port = ntohs(addr->sin6.sin6_port);
-    } break;
-    default:
-      dst[0] = '?';
-      dst[1] = 0x00;
-  }
-
-  static char buf[255];
-  snprintf(buf, sizeof(buf), "%s:%i", dst, port);
-  return buf;
-}
-
-int net_bind_udp(NetAddr* shost, int reuseport)
-{
-  int sd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  if (sd < 0) {
-    PFATAL("socket()");
-  }
-
-  int one = 1;
-  int r = setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char*)&one, sizeof(one));
-  if (r < 0) {
-    PFATAL("setsockopt(SO_REUSEADDR)");
-  }
-
-  if (reuseport) {
-    one = 1;
-    r = setsockopt(sd, SOL_SOCKET, SO_REUSEPORT, (char*)&one, sizeof(one));
-    if (r < 0) {
-      PFATAL("setsockopt(SO_REUSEPORT)");
-    }
-  }
-
-  if (bind(sd, shost->addr, shost->addr_len) < 0) {
-    PFATAL("bind()");
-  }
-
-  return sd;
-}
 
 static void* _thread_start(void* userdata)
 {
@@ -181,7 +209,7 @@ static void* _thread_start(void* userdata)
   return NULL;
 }
 
-Thread* thread_spawn(void (*callback)(void*), void* userdata)
+Thread* thread_spawn(void (*callback)(void*), void* userdata, int affinity)
 {
   Thread* thread = reinterpret_cast<Thread*>(calloc(1, sizeof(thread)));
   thread->callback = callback;
@@ -190,55 +218,25 @@ Thread* thread_spawn(void (*callback)(void*), void* userdata)
   if (r != 0) {
     PFATAL("pthread_create()");
   }
+
+	cpu_set_t cpuset;
+	CPU_ZERO(&cpuset);
+	CPU_SET(affinity, &cpuset);
+	pthread_setaffinity_np(thread->thread_id, sizeof(cpuset), &cpuset);
+
   return thread;
-}
-
-int net_connect_udp(NetAddr* shost, int src_port)
-{
-  int sd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  if (sd < 0) {
-    PFATAL("socket()");
-  }
-
-  int one = 1;
-  int r = setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char*)&one, sizeof(one));
-  if (r < 0) {
-    PFATAL("setsockopt(SO_REUSEADDR)");
-  }
-
-  if (src_port > 1 && src_port < 65536) {
-    NetAddr src;
-    memset(&src, 0, sizeof(NetAddr));
-    char buf[32];
-    snprintf(buf, sizeof(buf), "0.0.0.0:%d", src_port);
-    parse_addr(&src, buf);
-    if (bind(sd, src.addr, src.addr_len) < 0) {
-      PFATAL("bind()");
-    }
-  }
-
-  if (-1 == connect(sd, shost->addr, shost->addr_len)) {
-    /* is non-blocking, so we don't get error at that point yet */
-    if (EINPROGRESS != errno) {
-      PFATAL("connect()");
-      return -1;
-    }
-  }
-
-  return sd;
 }
 
 void thread_loop(void* userdata)
 {
   auto state = reinterpret_cast<State*>(userdata);
 
-  mmsghdr* messages = reinterpret_cast<mmsghdr*>(calloc(state->packets_in_buf, sizeof(mmsghdr)));
-  iovec* iovecs = reinterpret_cast<iovec*>(calloc(state->packets_in_buf, sizeof(iovec)));
+  auto messages = new mmsghdr[state->packets_in_buf];
+  auto iovecs = new iovec[state->packets_in_buf];
 
-  int fd = net_connect_udp(state->target_addr, state->src_port);
+  int fd = net::connect_udp(*state->target_addr, state->src_port);
 
-  int i;
-  for (i = 0; i < state->packets_in_buf; i++) {
+  for (int i = 0; i < state->packets_in_buf; i++) {
     iovec* iovec = &iovecs[i];
     mmsghdr* msg = &messages[i];
 
@@ -271,6 +269,9 @@ void thread_loop(void* userdata)
       bytes += len;
     }
   }
+
+	delete[] messages;
+	delete[] iovecs;
 }
 
 int main(int argc, const char* argv[])
@@ -339,19 +340,19 @@ int main(int argc, const char* argv[])
 
   int payload_sz = payload.size();
 
-  NetAddr target_addr;
-  parse_addr(&target_addr, target);
+  net::Addr target_addr;
+  net::parse_addr(target_addr, target);
 
   printf(
    "Sending to '%s'\n"
    "\nsend buffer contians %d packets, size %d\n"
    "number of threads to spawn: %d\n",
-   addr_to_str(&target_addr),
+   net::addr_to_str(target_addr),
    packets_in_buf,
    payload_sz,
    send_threads);
 
-  State* array_of_states = reinterpret_cast<State*>(calloc(send_threads, sizeof(State)));
+  auto array_of_states = new State[send_threads];
 
   for (int t = 0; t < send_threads; t++) {
     State* state = &array_of_states[t];
@@ -361,10 +362,12 @@ int main(int argc, const char* argv[])
     state->payload_sz = payload_sz;
     state->src_port = 50000 + t;
     fprintf(stderr, "[*] Creating socket on port %d\n", state->src_port);
-    thread_spawn(thread_loop, state);
+    thread_spawn(thread_loop, state, t + 2);
   }
 
   getchar();
+
+	delete[] array_of_states;
 
   return 0;
 }
