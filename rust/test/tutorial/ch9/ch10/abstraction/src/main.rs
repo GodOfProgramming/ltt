16f52524ca4f3c416dc6b28e0700d11a62edbce9
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-03-19 05:10:46 -0400

    quick commit

---
+++ rust/test/tutorial/ch9/ch10/abstraction/src/main.rs
@@ -0,0 +1,49 @@
+fn largest(list: &[i32]) -> i32 {
+  let mut largest = list[0];
+
+  for &item in list {
+    if item > largest {
+      largest = item;
+    }
+  }
+
+  largest
+}
+
+fn largest_gen<T>(list: &[T]) -> T {
+  let mut largest = list[0];
+
+  for &item in list {
+    if item > largest {
+      largest = item;
+    }
+  }
+
+  largest
+}
+
+fn main() {
+  let v = vec![1, 2, 3];
+
+  let v_largest = largest(&v);
+
+  println!("The largest number in v is {}", v_largest);
+
+  let a = [1, 2, 3];
+
+  let a_largest = largest(&a);
+
+  println!("This largest number in a is {}", a_largest);
+
+  let number_list = vec![1, 2, 3];
+
+  let number_largest = largest_gen(&number_list);
+
+  println!("The largest number in number_list is {}", number_largest);
+
+  let char_list = vec!['a', 'b', 'c'];
+
+  let char_largest = largest_gen(&char_list);
+
+  println!("The largest number in char_largest is {}", char_largest);
+}
--- rust/test/tutorial/ch9/error/src/main.rs
+++ rust/test/tutorial/ch9/error/src/main.rs
@@ -1,5 +1,43 @@
+use std::fs;
 use std::fs::File;
+use std::io;
 use std::io::ErrorKind;
+use std::io::Read;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+  let f = File::open("hello.txt");
+
+  let mut f = match f {
+    Ok(file) => file,
+    Err(e) => return Err(e),
+  };
+
+  let mut s = String::new();
+
+  return match f.read_to_string(&mut s) {
+    Ok(_) => Ok(s),
+    Err(e) => Err(e),
+  };
+
+  // or
+
+  let mut f = File::open("hello.txt")?;
+  let mut s = String::new();
+  f.read_to_string(&mut s)?;
+  return Ok(s);
+
+  // or
+
+  let mut s = String::new();
+
+  File::open("hello.txt")?.read_to_string(&mut s)?;
+
+  return Ok(s);
+
+  // or
+
+  fs::read_to_string("hello.txt")
+}
 
 fn main() {
   let f = match File::open("hello.txt") {
