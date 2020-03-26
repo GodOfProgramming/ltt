d978c229d2f52f4239d86c6e499aaa3ecd0c97c9
godofprogramming <jon.p.dickert@gmail.com>
2020-03-26 11:46:58 -0400

    quick commit

--- c++/lib/epoch/include/epoch/clock.hpp
+++ c++/lib/epoch/include/epoch/clock.hpp
@@ -4,79 +4,95 @@
 namespace epoch
 {
 #if defined _WIN32
-	using Instant = std::chrono::steady_clock::time_point;
+  using Instant = std::chrono::steady_clock::time_point;
 #elif defined __linux__
-	using Instant = std::chrono::system_clock::time_point;
+  using Instant = std::chrono::high_resolution_clock::time_point;
 #endif
 
-	using InternalClock = std::chrono::high_resolution_clock;
-	using Nanosecond = std::chrono::nanoseconds;
-	using Microsecond = std::chrono::microseconds;
-	using Millisecond = std::chrono::milliseconds;
-	using Second = std::chrono::seconds;
+  using InternalClock = std::chrono::high_resolution_clock;
+  using Nanosecond = std::chrono::nanoseconds;
+  using Microsecond = std::chrono::microseconds;
+  using Millisecond = std::chrono::milliseconds;
+  using Second = std::chrono::seconds;
 
-	class Clock
-	{
-	public:
-		Clock() = default;
+  class Clock
+  {
+   public:
+    Clock();
 
-		/* Timestamps the clock */
-		void reset();
+    /* Timestamps the clock */
+    void reset();
 
-		/* Get how much time as elasped since starting */
-		template <typename UnitOfTime>
-		double elapsed();
+    /* Get how much time as elasped since starting */
+    template <typename U>
+    double elapsed();
 
-		/* Check if a time duration has passed */
-		template <typename UnitOfTime>
-		bool elapsed(double value);
+    /* Check if a time duration has passed */
+    template <typename U>
+    bool elapsed(double value);
 
-	private:
-		Instant mNow;
-		size_t mDelta;
+    /* Returns the number of seconds since the epoch */
+    template <typename U>
+    double unixTime();
 
-		template <typename T>
-		double diff();
-	};
+   private:
+    Instant mNow;
+    size_t mDelta;
 
-	inline void Clock::reset()
-	{
-		mNow = InternalClock::now();
-	}
+    template <typename T>
+    double diff();
+  };
 
-	template <>
-	inline double Clock::elapsed<Nanosecond>()
-	{
-		return diff<std::nano>();
-	}
+  inline Clock::Clock()
+  {
+    reset();
+  }
 
-	template <>
-	inline double Clock::elapsed<Microsecond>()
-	{
-		return diff<std::micro>();
-	}
+  inline void Clock::reset()
+  {
+    mNow = InternalClock::now();
+  }
 
-	template <>
-	inline double Clock::elapsed<Millisecond>()
-	{
-		return diff<std::milli>();
-	}
+  template <>
+  inline double Clock::elapsed<Nanosecond>()
+  {
+    return diff<std::nano>();
+  }
 
-	template <>
-	inline double Clock::elapsed<Second>()
-	{
-		return diff<std::ratio<1>>();
-	}
+  template <>
+  inline double Clock::elapsed<Microsecond>()
+  {
+    return diff<std::micro>();
+  }
 
-	template <typename UnitOfTime>
-	inline bool Clock::elapsed(double value)
-	{
-		return std::chrono::duration_cast<UnitOfTime>(InternalClock::now() - mNow).count() >= value;
-	}
+  template <>
+  inline double Clock::elapsed<Millisecond>()
+  {
+    return diff<std::milli>();
+  }
 
-	template <typename T>
-	inline double Clock::diff()
-	{
-		return std::chrono::duration<double, T>(InternalClock::now() - mNow).count();
-	}
+  template <>
+  inline double Clock::elapsed<Second>()
+  {
+    return diff<std::ratio<1>>();
+  }
+
+  template <typename U>
+  inline bool Clock::elapsed(double value)
+  {
+    return std::chrono::duration_cast<U>(InternalClock::now() - mNow).count() >= value;
+  }
+
+  template <typename T>
+  inline double Clock::unixTime()
+  {
+    const auto seconds = std::chrono::duration_cast<T>(InternalClock::now().time_since_epoch());
+    return seconds.count();
+  }
+
+  template <typename T>
+  inline double Clock::diff()
+  {
+    return std::chrono::duration<double, T>(InternalClock::now() - mNow).count();
+  }
 }  // namespace epoch
--- c++/lib/epoch/spec/clock.spec.cpp
+++ c++/lib/epoch/spec/clock.spec.cpp
@@ -1,21 +1,43 @@
 #include <epoch/clock.hpp>
 #include <thread>
 
-#include <cspec.hpp>
+#include <cspec/cspec.hpp>
 
 using namespace std::chrono_literals;
 
 Eval(Clock)
 {
-    Describe("class Clock", [] {
-        Context("waiting 1 second", [] {
-            It("waits", [] {
-                epoch::Clock clock;
-                clock.reset();
-                std::this_thread::sleep_for(1s);
-                Expect(clock.elapsed<epoch::Second>(1)).toEqual(true);
-            });
-        });
-    });
-}
+  Describe("class Clock", [] {
+    Context("waiting 1 second", [] {
+      It("waits for a second", [] {
+        epoch::Clock clock;
+        std::this_thread::sleep_for(1s);
+        Expect(clock.elapsed<epoch::Second>(1)).toEqual(true);
+        Expect(clock.elapsed<epoch::Millisecond>(1000)).toEqual(true);
+        Expect(clock.elapsed<epoch::Microsecond>(1000000)).toEqual(true);
+        Expect(clock.elapsed<epoch::Nanosecond>(1000000000)).toEqual(true);
 
+        Expect(clock.elapsed<epoch::Second>(2)).toEqual(false);
+        Expect(clock.elapsed<epoch::Millisecond>(2000)).toEqual(false);
+        Expect(clock.elapsed<epoch::Microsecond>(2000000)).toEqual(false);
+        Expect(clock.elapsed<epoch::Nanosecond>(2000000000)).toEqual(false);
+      });
+    });
+
+    Context("unixTime()", [] {
+      It("gets the right time", [] {
+        epoch::Clock clock;
+        auto seconds = clock.unixTime<epoch::Second>();
+        Expect(seconds > 0).toEqual(true);
+        auto millis = clock.unixTime<epoch::Millisecond>();
+        Expect(millis >= seconds * 1000).toEqual(true);
+        auto micros = clock.unixTime<epoch::Microsecond>();
+        Expect(micros >= millis * 1000).toEqual(true);
+        auto nanos = clock.unixTime<epoch::Nanosecond>();
+        Expect(nanos >= micros * 1000).toEqual(true);
+
+        Expect(seconds >= 1585237554).toEqual(true);
+      });
+    });
+  });
+}
