860a679c82b4acb345de668239210b35b2fbe24a
godofprogramming <jon.p.dickert@gmail.com>
2020-03-26 11:49:43 -0400

    quick commit

--- c++/lib/epoch/include/epoch/clock.hpp
+++ c++/lib/epoch/include/epoch/clock.hpp
@@ -25,22 +25,22 @@ namespace epoch
 
     /* Get how much time as elasped since starting */
     template <typename U>
-    double elapsed();
+    double elapsed() const;
 
     /* Check if a time duration has passed */
     template <typename U>
-    bool elapsed(double value);
+    bool elapsed(double value) const;
 
     /* Returns the number of seconds since the epoch */
     template <typename U>
-    double unixTime();
+    double unixTime() const;
 
    private:
     Instant mNow;
     size_t mDelta;
 
     template <typename T>
-    double diff();
+    double diff() const;
   };
 
   inline Clock::Clock()
@@ -54,44 +54,44 @@ namespace epoch
   }
 
   template <>
-  inline double Clock::elapsed<Nanosecond>()
+  inline double Clock::elapsed<Nanosecond>() const
   {
     return diff<std::nano>();
   }
 
   template <>
-  inline double Clock::elapsed<Microsecond>()
+  inline double Clock::elapsed<Microsecond>() const
   {
     return diff<std::micro>();
   }
 
   template <>
-  inline double Clock::elapsed<Millisecond>()
+  inline double Clock::elapsed<Millisecond>() const
   {
     return diff<std::milli>();
   }
 
   template <>
-  inline double Clock::elapsed<Second>()
+  inline double Clock::elapsed<Second>() const
   {
     return diff<std::ratio<1>>();
   }
 
   template <typename U>
-  inline bool Clock::elapsed(double value)
+  inline bool Clock::elapsed(double value) const
   {
     return std::chrono::duration_cast<U>(InternalClock::now() - mNow).count() >= value;
   }
 
   template <typename T>
-  inline double Clock::unixTime()
+  inline double Clock::unixTime() const
   {
     const auto seconds = std::chrono::duration_cast<T>(InternalClock::now().time_since_epoch());
     return seconds.count();
   }
 
   template <typename T>
-  inline double Clock::diff()
+  inline double Clock::diff() const
   {
     return std::chrono::duration<double, T>(InternalClock::now() - mNow).count();
   }
