7409f971daaf36b3715c9d3ec52b86b59004258a
godofprogramming <jon.p.dickert@gmail.com>
2020-04-06 12:10:48 -0400

    quick commit

---
+++ rust/test/tutorial/ch15/dereference/src/main.rs
@@ -0,0 +1,14 @@
+use dereference::MyBox;
+
+fn main() {
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
