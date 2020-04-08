17c83d16e8f5f95b5b6dfac6bc318b148868f2de
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-04-08 00:19:25 -0400

    quick commit

--- rust/test/tutorial/ch17/gui/src/lib.rs
+++ rust/test/tutorial/ch17/gui/src/lib.rs
@@ -26,3 +26,14 @@ impl Draw for Button {
     }
 }
 
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // cod to draw
+    }
+}
