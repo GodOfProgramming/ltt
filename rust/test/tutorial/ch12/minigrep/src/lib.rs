dcde56bcae00c0abc9edbfab4966acd3402f6999
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-04-11 02:40:49 -0400

    quick commit

--- rust/test/tutorial/ch12/minigrep/src/lib.rs
+++ rust/test/tutorial/ch12/minigrep/src/lib.rs
@@ -46,28 +46,7 @@ pub fn run(args: &Args) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-
-  results
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let query = query.to_lowercase();
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.to_lowercase().contains(&query) {
-      results.push(line);
-    }
-  }
-
-  results
+  vec![]
 }
 
 #[cfg(test)]
