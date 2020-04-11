b8a2c49499a0a9335e8b9a1cbbc5473c392e84e9
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-04-11 17:59:58 -0400

    quick commit

--- rust/test/tutorial/ch20/webserver/src/bin/main.rs
+++ rust/test/tutorial/ch20/webserver/src/bin/main.rs
@@ -3,45 +3,51 @@ use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
-use webserver::ThreadPool;
+use webserver::{ThreadPool, ThreadPoolCreationError::ZeroThreads};
 
 fn main() {
-  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-  let pool = ThreadPool::new(4);
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = match ThreadPool::new(4) {
+        Ok(tp) => tp,
+        Err(e) => match e {
+            ZeroThreads(e) => panic!(e),
+        }
+    };
 
-  for stream in listener.incoming() {
-    let stream = stream.unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
 
-    handle_connection(stream);
-    pool.execute(|| {});
-  }
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-  let mut buffer = [0; 512];
+    let mut buffer = [0; 512];
 
-  stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).unwrap();
 
-  let resp = if buffer.starts_with(get("/").as_bytes()) {
-    let html = fs::read_to_string("public/index.html").unwrap();
-    response(200, "OK", &html)
-  } else if buffer.starts_with(get("/sleep").as_bytes()) {
-    thread::sleep(Duration::from_secs(5));
-    let html = fs::read_to_string("public/index.html").unwrap();
-    response(200, "OK", &html)
-  } else {
-    let html = fs::read_to_string("public/404.html").unwrap();
-    response(404, "NOT FOUND", &html)
-  };
+    let resp = if buffer.starts_with(get("/").as_bytes()) {
+        let html = fs::read_to_string("public/index.html").unwrap();
+        response(200, "OK", &html)
+    } else if buffer.starts_with(get("/sleep").as_bytes()) {
+        thread::sleep(Duration::from_secs(5));
+        let html = fs::read_to_string("public/index.html").unwrap();
+        response(200, "OK", &html)
+    } else {
+        let html = fs::read_to_string("public/404.html").unwrap();
+        response(404, "NOT FOUND", &html)
+    };
 
-  stream.write(resp.as_bytes()).unwrap();
-  stream.flush().unwrap();
+    stream.write(resp.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
 fn get(path: &'static str) -> String {
-  format!("GET {} HTTP/1.1\r\n", path)
+    format!("GET {} HTTP/1.1\r\n", path)
 }
 
 fn response(code: u32, code_desc: &'static str, body: &String) -> String {
-  format!("HTTP/1.1 {} {}\r\n\r\n{}", code, code_desc, body)
+    format!("HTTP/1.1 {} {}\r\n\r\n{}", code, code_desc, body)
 }
--- rust/test/tutorial/ch20/webserver/src/lib.rs
+++ rust/test/tutorial/ch20/webserver/src/lib.rs
@@ -1,13 +1,69 @@
-pub struct ThreadPool;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread::{self, JoinHandle};
+
+pub enum ThreadPoolCreationError {
+  ZeroThreads(&'static str),
+}
+
+pub struct ThreadPool {
+  threads: Vec<Worker>,
+  sender: mpsc::Sender<Job>,
+}
 
 impl ThreadPool {
-  pub fn new(count: u32) -> ThreadPool {
-    ThreadPool {}
+  /// Create a new Thread Pool
+  ///
+  /// The count is the number of threads in the pool.
+  ///
+  /// # Panics
+  ///
+  /// The 'new' function will panic if the count is zero.
+  pub fn new(count: usize) -> Result<ThreadPool, ThreadPoolCreationError> {
+    if count == 0 {
+      Err(ThreadPoolCreationError::ZeroThreads(
+        "Cannot create a thread pool with zero threads",
+      ))
+    } else {
+      let (sender, receiver) = mpsc::channel();
+      let receiver = Arc::new(Mutex::new(receiver));
+
+      let mut threads = Vec::with_capacity(count);
+
+      for id in 0..count {
+        threads.push(Worker::new(id, Arc::clone(&receiver)));
+      }
+
+      Ok(ThreadPool { threads, sender })
+    }
   }
 
-  pub fn execute<T>(&self, _: T)
+  pub fn execute<T>(&self, f: T)
   where
-    T: Fn(),
+    T: FnOnce() + Send + 'static,
   {
+    let job = Box::new(f);
+
+    self.sender.send(job).unwrap();
   }
 }
+
+struct Worker {
+  id: usize,
+  handle: JoinHandle<()>,
+}
+
+impl Worker {
+  pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    let handle = thread::spawn(move || loop {
+      let job = receiver.lock().unwrap().recv().unwrap();
+
+      println!("Worker {} got a job; executing.", id);
+
+      job();
+    });
+
+    Worker { id, handle }
+  }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
