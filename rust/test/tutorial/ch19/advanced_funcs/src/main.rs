23c672b1ab506d3cf6271c88374e41b160844280
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-04-11 13:04:31 -0400

    quick commit

--- rust/test/tutorial/ch19/advanced_funcs/src/main.rs
+++ rust/test/tutorial/ch19/advanced_funcs/src/main.rs
@@ -6,8 +6,16 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
     f(arg) + f(arg)
 }
 
+fn return_func() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
 fn main() {
     let answer = do_twice(add_one, 5);
 
     println!("The answer is: {}", answer);
+
+    let f = return_func();
+
+    (*f)(1);
 }
