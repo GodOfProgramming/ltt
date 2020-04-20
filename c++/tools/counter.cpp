5359058a73eb7c731a305e2cd89d789313a2bcd4
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-04-20 12:12:58 -0400

    quick commit

---
+++ c++/tools/counter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    unsigned int counter = 0;
+
+    while (true) {
+        std::cout << counter;
+        getchar();
+				counter++;
+    }
+
+    return 0;
+}
