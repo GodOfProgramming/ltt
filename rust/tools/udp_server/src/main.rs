465f19a7581cf7d0e3504382bad67e618da6f251
godofprogramming <jon.p.dickert@gmail.com>
2020-04-21 17:04:14 -0400

    added packet type and master token decryption to udp server

--- rust/tools/udp_server/src/main.rs
+++ rust/tools/udp_server/src/main.rs
@@ -1,20 +1,62 @@
 use std::net::UdpSocket;
-use std::str;
+use std::sync::mpsc;
+use std::thread;
+use byteorder::{BigEndian, ReadBytesExt};
 
-fn main() -> std::io::Result<()> {
+struct Fragment {
+  packet_type: u8,
+  address: [u8; 19],
+  timestamp: u64,
+  mac: [u8; 32],
+}
+
+impl Fragment {
+  fn new(packet_type: u8, address: [u8; 19], timestamp: u64, mac: [u8; 32]) -> Fragment {
+    Fragment {
+      packet_type,
+      address,
+      timestamp,
+      mac,
+    }
+  }
+}
+
+fn recv(sender: mpsc::Sender<Vec<u8>>) -> std::io::Result<()> {
   let socket = UdpSocket::bind("127.0.0.1:51034")?;
 
   let mut buf = [0; 1500];
   while let Ok((amt, src)) = socket.recv_from(&mut buf) {
     let buf = &mut buf[..amt];
-    let data = str::from_utf8(&buf).unwrap();
-    println!("request: {}", data);
-    let mut v = Vec::from(buf);
-    v.reverse();
-    v.push(49);
-    v.reverse();
-    socket.send_to(&v, src)?;
+    let buf = Vec::from(buf);
+    sender.send(buf).unwrap();
+    socket.send_to("placeholder".as_bytes(), src).unwrap();
   }
+
+  Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+  let (sender, receiver) = mpsc::channel::<Vec<u8>>();
+
+  let t = thread::spawn(move || {
+    let msg = receiver.recv().unwrap();
+
+    let mut addr = [0; 19];
+    addr.copy_from_slice(&msg[1..20]);
+
+    let timestamp = (&msg[20..28]).read_u64::<BigEndian>().unwrap();
+
+    let mut mac = [0; 32];
+    mac.copy_from_slice(&msg[28..60]);
+
+    let frag = Fragment::new(msg[0], addr, timestamp, mac);
+
+    println!("Got fragment! Packet type: {}", frag.packet_type);
+  });
+
+  recv(sender)?;
+
+  t.join().unwrap();
 
   Ok(())
 }
