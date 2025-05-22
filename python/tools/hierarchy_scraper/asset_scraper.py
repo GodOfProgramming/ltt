05d5cb5d215d872c96591976c8f77aecc1a3b4f8
Jonathan Dickert <jonathan.dickert@protonmail.com>
2025-05-21 21:40:11 -0400

    quick commit

--- python/tools/hierarchy_scraper/asset_scraper.py
+++ python/tools/hierarchy_scraper/asset_scraper.py
@@ -1,4 +1,9 @@
 import unreal
 
-for cls in unreal.ClassIterator(unreal.Object):
-  print(cls.get_class_path_name())
+asset_registry = unreal.AssetRegistryHelpers.get_asset_registry()
+
+for untyped_asset in asset_registry.get_all_assets():
+  asset: unreal.AssetData = untyped_asset
+  parent = asset.get_tag_value('ParentClass')
+  if parent is not None:
+    unreal.log(f"Parent of {asset.asset_name}: {parent}")
